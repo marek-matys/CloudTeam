0cec24ffe001f597c9b2587aac3c31b91611de76
marek-matys <marek.matys@gmail.com>
2021-02-05 12:50:27 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php echo '<h1 class="contentpagetitle">Tytuł strony</h1>' ?> 
+<?php echo '<h1 class="contentpagetitle">Na życzeni Panów Szacownych ! :)</h1>' ?> 
 <?php get_loaded_extensions() ?> 
 <?php phpinfo() ?> 
